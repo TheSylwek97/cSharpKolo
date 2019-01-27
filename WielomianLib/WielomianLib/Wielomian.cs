8c7b8dd478cd2e1e2ed699a4ee32781e8d70121e
TheSylwek97 <38591912+TheSylwek97@users.noreply.github.com>
2019-01-27 13:37:50 +0100

    no message

---
+++ WielomianLib/WielomianLib/Wielomian.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    public class Wielomian
+    {
+        private int[] a;
+        public int Stopien => a.Length - 1; //to tylko get
+
+        public Wielomian()
+        {
+            a = new int[1] { 0 };
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i <= Stopien - 1; i++)
+            {
+                s += a[i] + "x^" + i + " + ";
+            }
+            return s + a[Stopien] + "x^" + Stopien;
+        }
+
+        public Wielomian(params int[] wspolczynniki)
+        {
+            a = new int[wspolczynniki.Length]; //nie wlono a=wspol... bo nie będzie niezmienniczości
+            Array.Copy(wspolczynniki, a, wspolczynniki.Length); //a chyba nie może być pusta
+            Array.Reverse(a);
+        }
+    }
+}
